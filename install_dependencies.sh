a0c4952c37bb33b93015dd444e9797a7da09d899
jshleap <jshleap@squalus.org>
2019-08-16 18:03:21 -0400

    adding dependencies installation

---
+++ install_dependencies.sh
@@ -0,0 +1,17 @@
+cwd=${PWD}
+
+echo "Preparing cutadapt"
+python -m pip install --user --upgrade cutadapt --user
+
+echo "Preparing vsearch"
+git clone https://github.com/torognes/vsearch.git
+cd vsearch
+./autogen.sh
+./configure --prefix=${PWD} --exec-prefix=${PWD}
+make
+echo "export PATH=$PATH:$PWD" >> ~/.bashrc
+echo "Setting up seqkit, pear and usearch "
+cd executables_linux_64
+echo "export PATH=$PATH:$PWD" >> ~/.bashrc
+source ~/.bashrc
+
